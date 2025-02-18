591f857f6681c0a3215e5cc43f023f6d10a571f1
XPOW <zug3049@gmail.com>
2025-02-18 22:21:58 +0900

    Fix: playerControl

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float moveSpeed;
-    // Start is called before the first frame update
+    protected Rigidbody2D rigid; // 리지드바디 이름 선언
+    protected Vector2 movementDirection = Vector2.zero; // 방향값 선언 및 초기화
+    public Vector2 MovementDirection { get { return movementDirection; } }
     void Start()
     {
-        
+       rigid = GetComponent<Rigidbody2D>(); // 리지드 바디 가져와서 쓸게요
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() // 유니티 내에 있는 InputManager의 "Horizontal"과 "vertical"키 값 사용
     {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Movement(moveInput.normalized); //방향을 정규화시켜준다
+    }
+    private void Movement(Vector2 direction)
+    { // 방향에 따른 값의 5를 곱하여 리지드 바디 만큼의 velocity를 속도로 설정함
 
-        transform.Translate(new Vector2 (inputX, inputY)*Time.deltaTime*moveSpeed);
+        direction = direction * 5;
+
+        rigid.velocity = direction;
     }
 }
+
