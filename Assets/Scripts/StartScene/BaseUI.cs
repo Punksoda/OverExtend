4767ab29fdbbe0504c43dcc5ef41ea450c1cb0f0
XPOW <zug3049@gmail.com>
2025-02-20 15:14:10 +0900

    Chore: BaseUI

--- Assets/Scripts/StartScene/BaseUI.cs
+++ Assets/Scripts/StartScene/BaseUI.cs
@@ -8,8 +8,8 @@ public abstract class BaseUI : MonoBehaviour
 
     public virtual void Init(UIManager uiManager)
     {
-        // canvas를 하이러라키 창에서 자동 생성되는 evevntManager를 무시하고 UI를 따로 프리팹 화 시켰습니다
-        // 따로 canvas를 생성해서 eventmanager를 만들었어야 했는데, 이미 Home UI에 들어가 생성이 되지 않습니다;;
+        // canvas를 하이러라키 창에서 자동 생성되는 evevntSystemdmf 무시하고 UI를 따로 프리팹 화 시켰습니다
+        // 따로 canvas를 생성해서 eventSystem을 만들었어야 했는데, 이미 Home UI에 들어가 생성이 되지 않습니다;;
         this.uiManager = uiManager;
     }
 }
